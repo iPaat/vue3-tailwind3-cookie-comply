f555190a4efb5179eaaa5d64aff8c000b6cd9dfb
iPaat <conrad.patrick.1@gmail.com>
2022-08-18 01:15:03 +0200

    Optimize build.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
+import minimist from 'minimist';
+
+const { f } = minimist(process.argv.slice(2));
 
 export default defineConfig({
   plugins: [vue()],
@@ -13,8 +16,10 @@ export default defineConfig({
   },
 
   build: {
+    emptyOutDir: false,
+
     lib: {
-      formats: ['es', 'umd'],
+      formats: f === 'iife' ? ['iife'] : ['es', 'umd'],
       entry: resolve(__dirname, 'src', 'entry.esm.ts'),
       name: 'VueCookieComply',
       fileName: (format) => `vue3-tailwind3-cookie-comply.${format}.js`,
